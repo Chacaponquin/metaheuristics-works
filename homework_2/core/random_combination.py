a39955c677fd16347e6b27eb660cc3d7838a83e5
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-02-28 16:54:07 -0500

    Update

--- homework_2/core/random_combination.py
+++ homework_2/core/random_combination.py
@@ -2,9 +2,9 @@ import random
 
 
 def random_combination(solution: list[float], solution_alt: list[float]):
-    l = len(solution)
-    for i in range(0, len(solution)):
-        if random.randint(0, l):
+    length = len(solution)
+    for i in range(len(solution)):
+        if random.randint(0, length):
             solution[i] = solution_alt[i]
 
     return solution
